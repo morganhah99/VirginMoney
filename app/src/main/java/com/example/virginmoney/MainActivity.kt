4b016c87ff9278092cae5dbe1431756d6f0d1061
morganhah99 <mjh58@njit.edu>
2024-05-03 11:24:20 -0400

    Finished bottom navigation bar

--- app/src/main/java/com/example/virginmoney/MainActivity.kt
+++ app/src/main/java/com/example/virginmoney/MainActivity.kt
@@ -5,16 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.virginmoney.databinding.ActivityMainBinding
+import com.example.virginmoney.ui.list.people.PeopleFragment
+import com.example.virginmoney.ui.list.rooms.RoomsFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(PeopleFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.people -> replaceFragment(PeopleFragment())
+                R.id.rooms -> replaceFragment(RoomsFragment())
+
+                else -> {
+
+                }
+
+            }
+            true
         }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/virginmoney/ui/list/people/PeopleFragment.kt
@@ -0,0 +1,21 @@
+package com.example.virginmoney.ui.list.people
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.virginmoney.R
+
+
+class PeopleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_people, container, false)
+    }
+
+
+}
